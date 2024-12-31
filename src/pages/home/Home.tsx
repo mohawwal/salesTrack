7cbe76e21eba42896c684345b856a91cd087eec5
awwal <leyeawwal@gmail.com>
2024-12-31 18:03:12 +0100

    design home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,22 +1,31 @@
 // import { Button } from "../../components/ui/button"
 // import { Link } from "react-router-dom"
 
-import HomeBody from "./HomeBody";
 import HomeNav from "./HomeNav";
 
 export const Home = () => {
-
 	return (
 		<div className="w-full bg-black">
 			<div className="w-[95%] m-auto">
 				<div className="h-[100vh] pt-4 relative">
-					{/* <div className="w-full h-full absolute opacity-10 flex items-center justify-center">
-						<div className=" bg-red-400 w-[700px] h-[700px]"></div>
-					</div> */}
 					<HomeNav />
-					<HomeBody />
+					<div>
+						<div className="flex mt-[150px] flex-col h-full">
+						<div className="font-thin">
+  <h1 className="font-Funnel text-center text-[clamp(3rem,_7vw,_6rem)] leading-none">
+    Sales <br /> Ecosystem
+  </h1>
+</div>
+
+							<div className="font-RobotoFlex text-center py-10 w-[90%] mx-auto text-[13.5px] sm:w-[70%] md:w-[55%] nv:w-[40%]">
+								Lorem ipsum dolor sit amet consectetur adipisicing elit.
+								Tempora, deleniti sed adipisci fuga itaque unde nesciunt, error
+								mollitia quibusdam repellat necessitatibus tenetur quidem,
+								inventore voluptatum blanditiis eligendi facilis eos dolorem!
+							</div>
+						</div>
+					</div>
 				</div>
-				<div>section 2</div>
 			</div>
 		</div>
 	);
--- src/pages/home/HomeNav.tsx
+++ src/pages/home/HomeNav.tsx
@@ -1,27 +1,36 @@
-import { Flower2 } from "lucide-react";
+import { AlignLeft, Flower2 } from "lucide-react";
 import { Link } from "react-router-dom";
 
 const HomeNav = () => {
 	return (
 		<div className="flex justify-between w-full">
-			<div className="w-[70%] flex items-center gap-[40px] cursor-pointer text-[13.8px]">
-				<div className="pt-4 flex items-start gap-1 px-2 text-[20px] text-foreground">
-					<Flower2 className="w-8 h-8 pb-1" />
-					<div className="font-RobotoFlex">
+			<div className="w-[70%] flex items-center gap-[20px] cursor-pointer text-[13.8px]">
+				<div className="hidden md:flex items-start gap-1 px-2 text-[15px] text-foreground mt-2">
+					<Flower2 className="w-7 h-7 pb-1" />
+					<div className="font-RobotoFlex font-semibold">
 						SALES <span className="text-primary">DETAL</span>
 					</div>
 				</div>
-				<div className="flex gap-8 font-RobotoFlex mt-4">
-					<p className="hover:underline">Learn</p>
-					<p className="hover:underline">Build</p>
-					<p className="hover:underline">Tools</p>
-					<p className="hover:underline">Media</p>
-					<p className="hover:underline">Jobs</p>
+
+				<div className="hidden md:flex gap-8 font-RobotoFlex">
+					<p className="hover:underline underline-offset-4">Learn</p>
+					<p className="hover:underline underline-offset-4">Build</p>
+					<p className="hover:underline underline-offset-4">Tools</p>
+					<p className="hover:underline underline-offset-4">Media</p>
+					<p className="hover:underline underline-offset-4">Jobs</p>
+				</div>
+
+				<div className="flex md:hidden">
+					<AlignLeft className="text-[20px] text-foreground" />
 				</div>
 			</div>
-			<div className="flex items-center bg-white text-black py-[2%] px-[3%] rounded-[100px]">
-				<Link to='/dashboard' className="text-center w-[100%] font-RobotoFlex text-[13px]">
-					What is SD?
+
+			<div className="border-2 border-primary flex items-center bg-white text-black py-[1.8%] px-[2.8%] rounded-[100px]">
+				<Link
+					to="/dashboard"
+					className="font-semibold text-center w-[100%] font-RobotoFlex text-[13px]"
+				>
+					Get Started?
 				</Link>
 			</div>
 		</div>
